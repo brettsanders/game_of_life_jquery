882b155f6e5aea52abc785ca6d56255575d84777
Brett Sanders <brettrsanders@gmail.com>
2013-11-10 12:36:54 -0600

    add random population game board, placeRandomCells() and getCell()

--- gameOfLife.js
+++ gameOfLife.js
@@ -4,11 +4,6 @@ var chanceOfLiveCell = 0.5;
 var table;
 var cells;
 
-$(document).ready(function(){
-  table = $('#main');
-  initializeGame();
-});
-
 function initializeGame(){
   // create table of dimension x dimension
   var trHtml = [];
@@ -23,3 +18,36 @@ function initializeGame(){
   // instead of string, jQuery node to be used
   table.append($(trHtml));
 }
+
+function placeRandomCells(){
+  for (var y = 0; y < dimension; y++) {
+    for (var x = 0; x < dimension; x++) {
+      var cell = getCell(x, y);
+      if (Math.random () > chanceOfLiveCell) {
+        cell.addClass('alive');
+      } else {
+        cell.removeClass('alive');
+      }
+    }
+  }
+}
+
+function getCell(x, y) {
+  // board in theory endless, make sure to come back
+  // don't actually need these 4 lines to work
+  if (x >= dimension) { x = 0; }
+  if (y >= dimension) { y = 0; }
+  if (x < 0) { x = dimension - 1; }
+  if (y < 0) { y = dimension - 1; }
+  // cells is a 1 dimensional array of all cells
+  // loops by a factor of y
+  return $(cells[y * dimension + x]);
+}
+
+$(document).ready(function(){
+  table = $('#main');
+  initializeGame();
+  // cache the cells
+  cells = table.find('td');
+  placeRandomCells();
+});
